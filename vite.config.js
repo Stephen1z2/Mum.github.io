1e102f4aeb5ee0d2d307f593d225b9a7d5fd510d
Stephen Lewis <123605823+Stephen1z2@users.noreply.github.com>
2025-05-11 20:35:47 -0700

    first

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Mum.github.io/', // Replace with your GitHub repo name if deploying to GitHub Pages
   plugins: [react()],
 })
